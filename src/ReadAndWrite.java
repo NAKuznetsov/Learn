87bbcde9fb139302642bcc06d8e92342286ffb08
NAKuznetsov <dankeschmidt@gmail.com>
2019-05-31 23:28:52 +0300

    Доделал задание №18

--- src/ReadAndWrite.java
+++ src/ReadAndWrite.java
@@ -4,17 +4,28 @@ public class ReadAndWrite {
     public static void main(String[] args) {
         String r;
         String w;
+        int count = 0;
+
         try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nikita\\Desktop\\readandwrite.txt")))
         {
+            LineNumberReader lineReader = new LineNumberReader(br);
+            while (lineReader.readLine() != null)
+            {
+                count++;
+            }
+
             while ((r=br.readLine())!=null)
             {
                 System.out.println(r);
             }
+
         }
         catch (IOException exc)
         {
             System.out.println("Ошибка ввода-вывода: " + exc);
         }
+
+        System.out.println("Количество строк в файле = " + count);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Признак конца ввода - строка 'stop' ");
@@ -31,6 +42,7 @@ public class ReadAndWrite {
                 fw.write(w);
             }
             while (w.compareTo("stop") !=0);
+
         }
         catch (IOException exc)
         {
